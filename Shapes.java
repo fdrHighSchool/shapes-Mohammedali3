410c27d72cf87ec23cdc7570a860e848126e2a65
Mohammed Ali <mohammeda258@nycstudents.net>
2020-10-13 15:09:01 -0400

    added my code

--- Shapes.java
+++ Shapes.java
@@ -1,25 +1,23 @@
-import java.util.*;
+import java.until.*;
 
-public class Shapes{
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter the side length: ");
-    double side = input.nextDouble();
-    square(side);
-  }
+ public class Shapes{
+   public static void main(String[] args){
+     square();
+     triangle();
 
-  /*
-   * N: square
-   * P: to calculate the area of any square
-   * I: a side measure (double)
-   * R:
-   */
-  public static void square(double s){
-    //side times side
-    double a = s * s;
+     public static void square(){
+       double Squareside = 10;
+       double ans = Squareside*Squareside;
+       system.out.println(ans);
 
-    //generate some output
-    System.out.println("A square with side " + s + " has an area of " + a);
-  }//close square method
 
-}
+       public static void triangle(){
+         double H = 10;
+         double B = 9;
+         double Half = 0.5;
+         double ans2 = Half*B*H;
+          system.out.println(ans2);
+       }
+     }
+   }
+ }
